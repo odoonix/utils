4db5ac640f2edca1f627b3b468013f9ffb5014cf
maso <mostafa.barmshory@gmail.com>
2025-02-03 08:17:29 +0330

    [fix] load repos from default config

--- src/otoolbox/repositories/__init__.py
+++ src/otoolbox/repositories/__init__.py
@@ -19,6 +19,9 @@ RESOURCE_REPOSITORIES_PATH = "data/repositories.json"
 # init
 ###################################################################
 
+# Load repositories as resources
+# If no repository found, then default list will be loaded
+
 
 def _load_repositories():
     reposiotires_path = env.get_workspace_path(REPOSITORIES_PATH)
@@ -28,21 +31,39 @@ def _load_repositories():
             data = f.read()
 
     if not data:
-        with env.resource_string(reposiotires_path, 'w') as f:
-            data = f.read()
+        data = env.resource_string(RESOURCE_REPOSITORIES_PATH)
     repo_list = json.loads(data)
+    workspaces = []
     for item in repo_list:
         env.add_resource(
             path="{}/{}".format(item["workspace"], item["name"]),
             title="Git repository: {}/{}".format(item["workspace"], item["name"]),
-            description="""
-                    Automaticaly added resources from git.
-                """,
+            description="""Automaticaly added resources from git.""",
             constructors=[
                 git.git_clone
             ],
             destructors=[],
             validators=[]
+        )
+        if item["workspace"] not in workspaces:
+            workspaces.append(item["workspace"])
+
+    for workspace in workspaces:
+        env.add_resource(
+            priority=11,
+            path="{}".format(workspace),
+            title="Git workspace: {}".format(workspace),
+            description="""Automaticaly added resources from git workspace.""",
+            constructors=[
+                utils.makedir
+            ],
+            destructors=[
+                utils.delete_dir
+            ],
+            validators=[
+                utils.is_dir,
+                utils.is_readable
+            ]
         )
 
 
@@ -67,3 +88,4 @@ def init():
             ]
         )
      )
+    _load_repositories()
